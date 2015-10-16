7a85eec5398ca6c1f2c764df1f6ec03f905a35a0
unknown <rtang91@gmail.com>
2015-10-15 20:44:39 -0400

    add score board and restart button

--- js/app.js
+++ js/app.js
@@ -2,33 +2,44 @@
 var Enemy = function(x,y) {
     this.x = x;
     this.y = y;
-    this.speed = Math.floor(Math.random() * 3 +1);// Variables applied to each of our instances go here,
-
-  // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    this.speed = Math.floor(Math.random() * 3 +1);// generates a random speed for the enemy
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+
+// Checks enemies for collisions
 Enemy.prototype.checkCollision = function() {
-    if (player.x < this.x + 60 && player.x + 65 > this.x && player.y < this.y + 50 && 50 + player.y > this.y) {
+    if (player.x < this.x + 60 && player.x + 60 > this.x && player.y < this.y + 60 && 60 + player.y > this.y) {
         player.x =200;
-        player.y =420;;
+        player.y =420;
+        player.lives -= 1;
+        var livesCount = document.getElementById('lives');
+        livesCount.innerHTML = "Lives: " + player.lives;
+        if (player.lives===0) {
+            alert('Game Over');
+            allEnemies = [];
+        }
+
     }
 };
 
+//function to update the enemies location
 Enemy.prototype.update = function(dt) {
     this.x = this.x + this.speed;
     if (this.x > 450) {
         this.x = -50;
     }
-    this.checkCollision();// You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.checkCollision();
+};
+
+
+// Constructor function for player
+var Player = function(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+    this.score = 0;
+    this.lives = 5;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -36,14 +47,7 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
-var Player = function(x,y) {
-    this.x = x;
-    this.y = y;
-    this.sprite = 'images/char-boy.png'
-};
-
-
+// Function for updating player location
 Player.prototype.update = function() {
     if(this.x<0) {
         this.x=0;
@@ -53,16 +57,22 @@ Player.prototype.update = function() {
     }
     if(this.y>420){
         this.y=420;
-        this.score += 1;
     }
     if(this.y<0){
         this.y=420;
+        this.x=200;
+        this.score += 1;
+        var scoreCount = document.getElementById('score');
+        scoreCount.innerHTML = "Score: " + player.score;
     }
 };
 
+// Renders the player on the canvas
 Player.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+// How the player moves after certain keys are pressed
 Player.prototype.handleInput = function(key){
     switch (key) {
         case 'left': 
@@ -80,24 +90,17 @@ Player.prototype.handleInput = function(key){
         }
 
 };
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 
-var bug1 = new Enemy(0,50);// Now instantiate your objects.
+
+var bug1 = new Enemy(0,50);// Instantiate enemies
 var bug2 = new Enemy(-100,150);
 var bug3 = new Enemy(0, 250);
-var bug4 = new Enemy(-250, 200);
-var player = new Player(200,420);// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var allEnemies = [bug1, bug2, bug3, bug4];
+var player = new Player(200,420); // Place the player object in a variable called player
+var allEnemies = [bug1, bug2, bug3]; // Place all enemy objects in an array called allEnemies
 
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to the Player.handleInput() method
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -105,6 +108,20 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+var restartButton = document.getElementById('restartButton');
+// Restart button for restarting the game after player has lost all 5
+restartButton.onclick = function () {
+    var scoreCount = document.getElementById('score');
+    var livesCount = document.getElementById('lives');
+    player.lives = 5;
+    player.score = 0;
+    scoreCount.innerHTML = "Score: " + player.score;
+    livesCount.innerHTML = "Lives: " + player.lives;
+    allEnemies = [bug1, bug2, bug3];
+};s
+
+
+
